ed95b7351c0d833912a96add5ce1c8788e8f21c1
FDSoftware <dfrenoux@gmail.com>
2024-01-16 15:49:34 -0300

    wip fix fw crash

--- speeduino/init.cpp
+++ speeduino/init.cpp
@@ -299,7 +299,7 @@ void initialiseAll(void)
     {
       //First time running on this board
       resetConfigPages(); 
-      setPinMapping(3); //Force board to v0.4
+      setPinMapping(70); //Force board to v0.4
     }
     else { setPinMapping(configPage2.pinMapping); }
 
